7e013fd54b7b6e562f428850f02c7dc746252e54
Boi Schrader <boi1992@t-online.de>
2022-07-05 13:15:51 +0200

    language and numbers done

---
+++ solution.js
@@ -0,0 +1,14 @@
+let introSentence = "hello, my name is Fran and I am ";
+let age = 25;
+
+console.log(introSentence+age)
+
+// the + operator lets us combine 2 different variables, whether they are strings or numbers//
+
+var integer = "1005"
+var float = "10.05"
+
+console.log(Number(integer))
+console.log(parseInt(float))
+
+//
